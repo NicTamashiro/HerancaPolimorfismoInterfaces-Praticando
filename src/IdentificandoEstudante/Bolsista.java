d8f081e343bc423a1de161fd21bd79053b28cb86
Nicolas <adilsonq1976@gmail.com>
2026-07-07 10:23:51 -0300

    Exercicio Identificando Estudante

---
+++ src/IdentificandoEstudante/Bolsista.java
@@ -0,0 +1,7 @@
+package IdentificandoEstudante;
+
+public class Bolsista extends Aluno{
+    public Bolsista(String nome) {
+        super(nome, "bolsista");
+    }
+}
---
+++ src/IdentificandoEstudante/Main.java
@@ -0,0 +1,12 @@
+package IdentificandoEstudante;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("Fernanda", "regular");
+        Bolsista aluno2 = new Bolsista("Lucas");
+
+        aluno1.identificar();
+        aluno2.identificar();
+    }
+}
+
