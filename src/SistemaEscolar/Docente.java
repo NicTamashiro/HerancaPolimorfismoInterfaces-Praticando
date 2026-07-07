2cf3338dbbb2136550396c0197371efed2ba3f0a
Nicolas <adilsonq1976@gmail.com>
2026-07-07 10:30:04 -0300

    Exercicio Sistema Escolar

---
+++ src/SistemaEscolar/Docente.java
@@ -0,0 +1,15 @@
+package SistemaEscolar;
+
+public class Docente extends Pessoa{
+    private String disciplina;
+
+    public Docente(String nome, int idade, String disciplina) {
+        super(nome, idade);
+        this.disciplina = disciplina;
+    }
+
+    @Override
+    public void exibirDados() {
+        System.out.println("Docente: " + getNome() + " - Idade: " + getIdade() + " - Disciplina: " + disciplina);
+    }
+}
