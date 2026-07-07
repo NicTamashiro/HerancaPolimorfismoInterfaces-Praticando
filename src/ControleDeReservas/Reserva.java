ac8b698a4339aeb738641e46fbb612b3338eff36
Nicolas <adilsonq1976@gmail.com>
2026-07-07 11:07:50 -0300

    Exercicio Controle de Reservas

---
+++ src/ControleDeReservas/Reserva.java
@@ -0,0 +1,16 @@
+package ControleDeReservas;
+
+public class Reserva {
+
+    public void reservar(){
+        System.out.println("Reserva realizada");
+    }
+
+    public void reservar(String data){
+        System.out.println("Reserva feita para o dia " + data);
+    }
+
+    public void reservar(String data, int pessoas){
+        System.out.println("Reserva feita para o dia " + data + " para " + pessoas + " pessoas");
+    }
+}
---
+++ src/ControleDeReservas/ReservaVip.java
@@ -0,0 +1,7 @@
+package ControleDeReservas;
+
+public class ReservaVip extends Reserva{
+    public void reservar(){
+        System.out.println("Reserva VIP confirmada com atendimento exclusivo");
+    }
+}
